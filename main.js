17e12c4569bcc2488ddac8fc891b7992b27a9f9b
mukeshahairwar <77151840+mukeshahairwar@users.noreply.github.com>
2021-04-08 15:06:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function play1() {
+    document.getElementById("aud").play();
+}
+function date() {
+    document.getElementById("h").innerHTML =new Date().toDateString();
+}
+function play2() {
+    document.getElementById("aud2").play();
+}
